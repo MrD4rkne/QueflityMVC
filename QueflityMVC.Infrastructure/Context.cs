7bbf1ddc98cf15096f652156ecf33a8983c3e91d
Marcin Szopa <marcin.pawel.szopa@gmail.com>
2023-06-15 00:09:40 +0200

    Add project files.

---
+++ QueflityMVC.Infrastructure/Context.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueflityMVC.Infrastructure
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ QueflityMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QueflityMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
