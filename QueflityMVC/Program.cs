e2aa52208ba1b54fc8dff6a4c53012f4a2f22996
Marcin Szopa <marcinsz.pv@gmail.com>
2023-12-16 23:50:02 +0100

    Add docker support

--- QueflityMVC/Program.cs
+++ QueflityMVC/Program.cs
@@ -66,4 +66,16 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<QueflityMVC.Infrastructure.Context>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 app.Run();
