69d194c6286376f6c0450a77b28152d280c43b0c
Marcin Szopa <marcinsz.pv@gmail.com>
2024-03-16 18:05:42 +0100

    <feat> add action buttons for purchasable on dashboard

--- src/QueflityMVC/Setup/Secrets/EnviromentCredentialsProvider.cs
+++ src/QueflityMVC/Setup/Secrets/EnviromentCredentialsProvider.cs
@@ -8,7 +8,6 @@ public class EnviromentCredentialsProvider : IVariablesProvider
 
     public string? GetConnectionString()
     {
-        //return "Server=172.24.48.126;Database=QueflityDb;User Id=SA;Password=Lf5892dENz;MultipleActiveResultSets=true;TrustServerCertificate=true";
         return Environment.GetEnvironmentVariable(DB_CONNECTION_STRING);
     }
 
