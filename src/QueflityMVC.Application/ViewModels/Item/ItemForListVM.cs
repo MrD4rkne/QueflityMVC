baa16f5a965f05aeb11bb49819b8a185c706acbc
Marcin Szopa <marcinsz.pv@gmail.com>
2024-09-03 13:47:51 +0200

    feat: add Price when listing Items

--- src/QueflityMVC.Application/ViewModels/Item/ItemForListVM.cs
+++ src/QueflityMVC.Application/ViewModels/Item/ItemForListVM.cs
@@ -9,6 +9,8 @@ public record ItemForListVm : IMapFrom<Domain.Models.Item>
 
     public required string Name { get; set; }
 
+    public required decimal Price { get; set; }
+
     public required bool ShouldBeShown { get; set; }
 
     public ImageForListVm? Image { get; set; }
